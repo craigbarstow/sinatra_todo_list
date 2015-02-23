621ebbe28115fe0290a880a31a7ff1297ce3a0a8
Craig Barstow <craig.barstow.social@gmail.com>
2015-02-23 08:12:21 -0500

    added class to help with sql management

--- server.rb
+++ server.rb
@@ -1,6 +1,7 @@
 require 'csv'
 require 'sinatra'
 require 'pry'
+require './files/sql_helper.rb'
 
 enable :sessions
 
@@ -11,10 +12,9 @@ end
 post '/' do
   user_info = {}
   CSV.foreach('user_info.csv', headers: true) do |row|
-    user_info[row[1]] = {:name => row[0], :password => row[2], :list_file => row[3]}
+    user_info[row[0]] = {:password => row[1], :email=> row[2],:list_file => row[3]}
   end
   if user_info[params['username']][:password] == params['password']
-    session[:name] = user_info[params['username']][:name]
     session[:list_file] = user_info[params['username']][:list_file]
     redirect '/groceries'
   else
@@ -36,7 +36,7 @@ post '/new' do
     new_file = File.open("./lists/#{new_filename}", "w")
     new_file.close
     CSV.open('user_info.csv', "a") do |file|
-      file.puts([params['new_name'], params['new_username'], params['new_password'], new_filename])
+      file.puts([params['new_username'], params['new_password'], params['email'], new_filename])
     end
     redirect '/'
   end
@@ -46,7 +46,7 @@ end
 
 get '/groceries' do
   list_items = File.readlines("lists/#{session[:list_file]}")
-  erb :index, locals: { list_items: list_items, name: session[:name] }
+  erb :index, locals: { list_items: list_items }
 end
 
 post '/groceries' do
